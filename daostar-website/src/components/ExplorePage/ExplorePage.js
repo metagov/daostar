02831c47b290282d6ac3d6f7aeaca65b6ee983be
Isaac Patka <ipatka@gmail.com>
2023-03-22 19:09:57 -0400

    wip

--- daostar-website/src/components/ExplorePage/ExplorePage.js
+++ daostar-website/src/components/ExplorePage/ExplorePage.js
@@ -1,59 +1,54 @@
-import React, { useState } from 'react';
-import RegistrationCard from '../RegistrationCard/RegistrationCard';
-import { mockExploreData } from './mockExploreData';
-import './ExplorePage.css';
-import validator from 'validator';
-import { useQuery } from '@apollo/client';
-import REGISTRATIONS from './queries/registrations';
-import { InputGroup } from '@blueprintjs/core';
+import React, { useEffect, useState } from 'react'
+import RegistrationCard from '../RegistrationCard/RegistrationCard'
+import { mockExploreData } from './mockExploreData'
+import './ExplorePage.css'
+import validator from 'validator'
+import { useQuery } from '@apollo/client'
+import queries from './queries/registrations'
+import { InputGroup } from '@blueprintjs/core'
 
 export const filterRegistrations = (registration, filterVal = '') => {
     if (!registration.daoName) {
-        return false;
+        return false
     }
     if (filterVal !== '') {
-        return registration.daoName.toLowerCase().includes(filterVal.toLowerCase());
+        return registration.daoName.toLowerCase().includes(filterVal.toLowerCase())
     }
-    return true;
+    return true
 }
 
-const ExplorePage = ({
+const ExplorePage = ({}) => {
+    const [filterVal, setFilterVal] = useState('')
+    const onChangeFilter = (e) => setFilterVal(e.target.value)
 
-}) => {
+    const mainnetRes = useQuery(queries.REGISTRATIONS_MAINNET, { context: { apiName: 'mainnet' } })
+    const goerliRes = useQuery(queries.REGISTRATIONS_GOERLI, { context: { apiName: 'goerli' } })
+    console.log({ mainnetRes, goerliRes })
+    const { loading, error, data: mainnetData } = mainnetRes
+    const { loading: goerliLoading, error: goerliError, data: goerliData } = goerliRes
+    console.log({ error, loading, mainnetData })
+    console.log({ goerliError, goerliLoading, goerliData })
 
-    const [filterVal, setFilterVal] = useState('');
-    const onChangeFilter = (e) => setFilterVal(e.target.value);
+    if (error || goerliError) return 'error'
+    if (loading || goerliLoading) return 'loading...'
+    const registrationInstances = mainnetData.registrationInstances.concat(goerliData.registrationInstances)
 
-    const { loading, error, data } = useQuery(REGISTRATIONS);
-    if (error) return 'error';
-    if (loading) return 'loading...';
+    console.log({ registrationInstances })
 
-    const daoCards = data.registrationInstances
-                        .filter(reg => filterRegistrations(reg, filterVal))
-                        .map((registration, i) => {
-        return (
-            <RegistrationCard 
-                key={i}
-                {...registration}
-            />
-        )
-    })
+    const daoCards = registrationInstances
+        .filter((reg) => filterRegistrations(reg, filterVal))
+        .map((registration, i) => {
+            return <RegistrationCard key={i} {...registration} />
+        })
 
     return (
-        <div className='explore-page'>
-            <div className='filter'>
-                <InputGroup
-                    large
-                    placeholder='Filter DAOs...'
-                    value={filterVal}
-                    onChange={onChangeFilter}
-                />
+        <div className="explore-page">
+            <div className="filter">
+                <InputGroup large placeholder="Filter DAOs..." value={filterVal} onChange={onChangeFilter} />
             </div>
-            <div className='dao-cards'>
-                {daoCards}
-            </div>
+            <div className="dao-cards">{daoCards}</div>
         </div>
     )
 }
 
-export default ExplorePage;
+export default ExplorePage
--- daostar-website/src/components/ExplorePage/queries/registrations.js
+++ daostar-website/src/components/ExplorePage/queries/registrations.js
@@ -1,7 +1,23 @@
 import { gql } from "@apollo/client";
 
 const REGISTRATIONS = gql`
-    query Registrations {
+    query Registrations @api(contextKey: "apiName") {
+        registrationInstances(first: 5) {
+            id
+            daoAddress
+            daoURI
+            daoName
+            daoDescription
+            membersURI
+            proposalsURI
+            governanceURI
+            activityLogURI
+            registrationAddress
+        }
+    }
+`
+const REGISTRATIONS_MAINNET = gql`
+    query Registrations @api(name: mainnet) {
         registrationInstances(first: 5) {
             id
             daoAddress
@@ -17,4 +33,21 @@ const REGISTRATIONS = gql`
     }
 `
 
-export default REGISTRATIONS;
+const REGISTRATIONS_GOERLI = gql`
+    query Registrations @api(name: goerli) {
+        registrationInstances(first: 5) {
+            id
+            daoAddress
+            daoURI
+            daoName
+            daoDescription
+            membersURI
+            proposalsURI
+            governanceURI
+            activityLogURI
+            registrationAddress
+        }
+    }
+`
+
+export default {REGISTRATIONS, REGISTRATIONS_GOERLI, REGISTRATIONS_MAINNET}
