b7fc286e57da28db11ecd079462f81e63b517f14
0xRory <0x1rory@gmail.com>
2023-06-05 06:08:14 +0800

    Revert "Revert "feat:  Creating Nouns Endpoints""
    This reverts commit 2fcb0a4825203e2c5298299620e5bbe252107c97.

--- Implementations/API/backend/functions/config.ts
+++ Implementations/API/backend/functions/config.ts
@@ -25,3 +25,7 @@ export const gnosisApiConfig: { [key: string]: any } = {
 export const snapshotApiConfig: { [key: string]: any } = {
     '1': 'https://hub.snapshot.org/graphql'
 }
+
+export const nonusApiConfig: { [key: string]: any } = {
+    '1': 'https://api.thegraph.com/subgraphs/name/nounsdao/nouns-subgraph'
+}
