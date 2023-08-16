10a601ca4671f852d30e1e8c85e5862671c722c7
Rashmi-278 <rashmiabbigeri@gmail.com>
2023-08-16 22:57:56 +0530

    remove inactive members and update rebranded ones

--- daostar-website/src/components/Homepage/MemberLogos/MemberLogos.js
+++ daostar-website/src/components/Homepage/MemberLogos/MemberLogos.js
@@ -9,10 +9,8 @@ import _1hiveLogo from '../../../img/logos/1hive.png';
 import abridgedLogo from '../../../img/logos/abridged.jpeg';
 import agoraLogo from '../../../img/logos/agora.png';
 import aragonLogo from '../../../img/logos/aragon.png';
-import avenueLogo from '../../../img/logos/avenue.png';
 import balancerLogo from '../../../img/logos/balancer.png';
 import boardroomLogo from '../../../img/logos/boardroom.jpeg';
-import bribeLogo from '../../../img/logos/bribe.jpg';
 import ceramicLogo from '../../../img/logos/ceramic.png';
 import charmverseLogo from '../../../img/logos/charmverse.png';
 import coinbaseLogo from '../../../img/logos/coinbase.png';
@@ -22,7 +20,6 @@ import consensysLogo from '../../../img/logos/consensys.png';
 import consensysMeshLogo from '../../../img/logos/consensys-mesh.png';
 import curveLabsLogo from '../../../img/logos/curvelabs.jpeg';
 import daodaoLogo from '../../../img/logos/daodao.png';
-import daoMastersLogo from '../../../img/logos/daomasters.jpeg';
 import developerDAOLogo from '../../../img/logos/developerdao.jpg';
 import daoCollectiveLogo from '../../../img/logos/daoresearchcollective.png';
 import deepDAOlogo from '../../../img/logos/deepdao.jpeg';
@@ -39,10 +36,10 @@ import interchainLogo from '../../../img/logos/interchain.png';
 import klerosLogo from '../../../img/logos/kleros.png';
 import kycdaoLogo from '../../../img/logos/kycdao.jpg';
 import litProtocolLogo from '../../../img/logos/litprotocol.png';
-import logosLogo from '../../../img/logos/logos.jpg';
 import messariLogo from '../../../img/logos/messari.jpeg';
 import metacartelLogo from '../../../img/logos/metacartel.png';
 import metagovLogo from '../../../img/logos/metagov.png';
+import metropoisLogo from '../../../img/logos/metropolis.jpg'
 import mirrorLogo from '../../../img/logos/mirror.png';
 import nearLogo from '../../../img/logos/near.png';
 import olympusLogo from '../../../img/logos/olympusdao.jpeg';
@@ -80,10 +77,8 @@ const MemberLogos = (props) => {
             <a href="https://abridged.io/" target="_blank"><img src={abridgedLogo} alt="Abridged" /></a>
             <a href="https://agora.xyz/" target="_blank"><img src={agoraLogo} alt="Agora Space" /></a>
             <a href="https://aragon.org/" target="_blank"><img src={aragonLogo} alt="Aragon" /></a>
-            <a href="https://avenue.place/" target="_blank"><img src={avenueLogo} alt="Avenue" /></a>
             <a href="https://balancer.finance/" target="_blank"><img src={balancerLogo} alt="BalancerDAO" /></a>
             <a href="https://www.boardroom.info/" target="_blank"><img src={boardroomLogo} alt="Boardroom" /></a>
-            <a href="https://www.bribe.xyz/" target="_blank"><img src={bribeLogo} alt="Bribe" /></a>
             <a href="https://ceramic.network/" target="_blank"><img src={ceramicLogo} alt="Ceramic" /></a>
             <a href="https://charmverse.io/" target="_blank"><img src={charmverseLogo} alt="Charmverse" /></a>	
             <a href="https://www.coinbase.com/" target="_blank"><img src={coinbaseLogo} alt="Coinbase" /></a>
@@ -94,7 +89,6 @@ const MemberLogos = (props) => {
             <a href="https://mesh.xyz/" target="_blank"><img src={consensysMeshLogo} alt="Consensys Mesh" /></a>
             <a href="https://www.curvelabs.eu/" target="_blank"><img src={curveLabsLogo} alt="Curve Labs" /></a>
             <a href="https://daodao.zone/" target="_blank"><img src={daodaoLogo} alt="DAO DAO" /></a>
-            <a href="https://daomasters.xyz/" target="_blank"><img src={daoMastersLogo} alt="DAO Masters" /></a>
             <a href="https://www.developerdao.com/" target="_blank"><img src={developerDAOLogo} alt="Developer DAO" /></a>
             <a href="https://www.daocollective.xyz/" target="_blank"><img src={daoCollectiveLogo} alt="DAO Research Collective" /></a>
             <a href="https://daostack.io/" target="_blank"><img src={daoStackLogo} alt="DAOstack" /></a>
@@ -112,17 +106,16 @@ const MemberLogos = (props) => {
             <a href="https://kleros.io/" target="_blank"><img src={klerosLogo} alt="Kleros" /></a>
             <a href="https://kycdao.xyz/" target="_blank"><img src={kycdaoLogo} alt="kycDAO" /></a>
             <a href="https://litprotocol.com/" target="_blank"><img src={litProtocolLogo} alt="Lit Protocol" /></a>
-            <a href="https://logos.xyz/" target="_blank"><img src={logosLogo} alt="Logos" /></a>
             <a href="https://messari.io/" target="_blank"><img src={messariLogo} alt="Messari" /></a>
             <a href="https://metacartel.xyz/" target="_blank"><img src={metacartelLogo} alt="MetaCartel Ventures" /></a>
             <a href="https://metagov.org/" target="_blank"><img src={metagovLogo} alt="Metagov" /></a>
+            <a href="https://metropolis.sh/" target="_blank"><img src={metropoisLogo} alt="Orca Protocol" /></a>
             <a href="https://mirror.xyz/" target="_blank"><img src={mirrorLogo} alt="Mirror" /></a>
             <a href="https://github.com/MolochVentures/moloch" target="_blank"><img src={molochLogo} alt="Moloch" /></a>
             <a href="https://near.org/" target="_blank"><img src={nearLogo} alt="NEAR" /></a>
             <a href="https://www.olympusdao.finance/" target="_blank"><img src={olympusLogo} alt="OlympusDAO" /></a>
             <a href="https://openzeppelin.com/" target="_blank"><img src={openZepellinLogo} alt="OpenZeppelin" /></a>
             <a href="https://opolis.co/" target="_blank"><img src={opolisLogo} alt="Opolis" /></a>
-            <a href="https://www.orcaprotocol.org/" target="_blank"><img src={orcaLogo} alt="Orca Protocol" /></a>
             <a href="https://otherinter.net/" target="_blank"><img src={otherInternetLogo} alt="Other Internet" /></a>
             <a href="https://otterspace.xyz/" target="_blank"><img src={otterspaceLogo} alt="Otterspace" /></a>
             <a href="https://paladin.vote/" target="_blank"><img src={paladinLogo} alt="Paladin" /></a>
