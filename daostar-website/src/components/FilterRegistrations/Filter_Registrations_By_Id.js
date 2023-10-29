eb3535922f93a7d68222c7ea9c7814ad5aa7adb4
Rashmi V Abbigeri <rashmiabbigeri@gmail.com>
2023-10-29 21:43:19 +0000

    hide spam and test daos

--- daostar-website/src/components/FilterRegistrations/Filter_Registrations_By_Id.js
+++ daostar-website/src/components/FilterRegistrations/Filter_Registrations_By_Id.js
@@ -6,6 +6,18 @@ const registrationIdsToFilter = [
     '0xdad5df58927e1743a6f88ec280d6b3fb264b05d5', // Kiwi dao demo without issuer uri
     '0xa5aa339314402edb5db68c7e3d3a526cf0245bf9', // Kiwi dao
     '0xdeddc6cdd12d9a514b7af9d51df9a3e46be2ddef', // Test dao
+    '0x9189687fa2b38b944629cee8a7ebc84214793c2a', // Test PR 166
+    '0xf9b8d94e1d78ad8cb1d7a4800e88dacf0fd5dcc9', // Test DAO
+    '0xe6d0d13d7558655c1c64bc92ea0e892cb69c2d19', // Orchid DAO
+    '0x5f7d4e12665b9d0fc57df2b9a1e1da56e8e057f2', // Orchid DAO
+    '0x24da46eb403dc54ff2e7442d88c745ff1991b07d', // Orchid DAO
+    '0x80e645e468966981fe279a8ee67600695e3a7976', // CatToys DAO
+    '0x8028dfdb40f0d78c1ed34be025eb779fd280c86b', // Orchid DAO
+    '0x86d1d7a482376ad3d5ec322486824dd74c405048', // Orchid DAO
+    '0xad3d3158c3032a131e43eb94cc4dbcbefe14a9cf', // Orchid DAO
+    '0xae6af870b06c20c5a9eccf55b4b5b95b01c5364c', // Orchid DAO
+
+
 
 ];
 
