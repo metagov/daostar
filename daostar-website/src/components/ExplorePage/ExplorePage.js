70fb64c21e27674bad0785a0bf29d79ea206d042
Rashmi-278 <rashmiabbigeri@gmail.com>
2024-10-14 18:28:44 +0700

    all cards

--- daostar-website/src/components/ExplorePage/ExplorePage.js
+++ daostar-website/src/components/ExplorePage/ExplorePage.js
@@ -7,46 +7,38 @@ import { InputGroup, Button } from "@blueprintjs/core";
 import { filterEASbyAttester, filterEASbyId } from "../FilterRegistrations/Filter_Registrations_By_Id";
 import RegistrationCard from "../RegistrationCard/RegistrationCard";
 
-// Prelimnary check filter, if a DAO has no name, it won't be displayed
+// Preliminary check filter, if a DAO has no name or matches excluded addresses or words
 export const filterRegistrations = (registration, filterVal = "") => {
-  // Check if daoName is present and not empty
-  if (!registration.daoName || registration.daoName.trim() === "") {
+  const daoNameLower = registration.daoName?.toLowerCase() || "";
+  const excludedWords = ["scam", "test", "fuck"];
+  const excludedAddress = "0xdeb9e5915db81011c549799a9ea37ede4d72efba";
+
+  // Return false early if conditions are not met
+  if (!daoNameLower || registration.daoAddress?.toLowerCase() === excludedAddress) {
     return false;
   }
 
-  // Omit dev test address
-  if (registration.daoAddress === "0xDeb9e5915Db81011C549799A9EA37EdE4d72EFBA") {
+  // Check if daoName contains excluded words or does not match filter
+  if (excludedWords.some((word) => daoNameLower.includes(word))) {
     return false;
   }
 
-  const daoNameLower = registration.daoName.toLowerCase();
-  const filterWords = ["scam", "test", "fuck"];
-
-  // Check for excluded words
-  if (filterWords.some((filterWord) => daoNameLower.includes(filterWord))) {
-    return false;
-  }
-
-  // Check if a specific filter value is provided and included in the daoName
+  // Perform filtering if a filterVal is provided
   return filterVal === "" || daoNameLower.includes(filterVal.toLowerCase());
 };
 
-// Network Filter for EVM Chains
 export const NetworkFilterRegistrations = (registration, filterVal = "") => {
-  if (registration.registrationNetwork.id === filterVal) {
+  const excludedAddress = "0xdeb9e5915db81011c549799a9ea37ede4d72efba";
+
+  if (registration.registrationNetwork.id === filterVal) return true;
+
+  if (registration.registrationNetwork.daoAddress === excludedAddress) return false;
+
+  if (filterVal === "ethereum" && 
+      ["ethereum", "mainnet"].includes(registration.registrationNetwork.id)) {
     return true;
   }
-  if (registration.registrationNetwork.daoAddress === '0xDeb9e5915Db81011C549799A9EA37EdE4d72EFBA') {
-    return false;
-  }
-  if (filterVal === "ethereum") {
-    if (
-      registration.registrationNetwork.id === filterVal ||
-      registration.registrationNetwork.id === "mainnet"
-    ) {
-      return true;
-    }
-  }
+
   return false;
 };
 
@@ -82,10 +74,13 @@ const ExplorePage = ({
   const onChangeFilter = (e) => setFilterVal(e.target.value);
   const [networkFilter, setNetworkFilter] = useState("all");
 
-  console.log("Sunrise Instances");
-  console.log(sunriseInstances);
+  const isValidDaoURI = (daoURI) => {
+    return (
+      (daoURI.startsWith("ipfs://") && daoURI.length >= 53) ||
+      (daoURI.startsWith("https://ipfs.io/ipfs/") && daoURI.length >= 67)
+    );
+  };
 
-  // Network Filter for Juno, Stargaze and Osmosis
   const filteredRegistrations = (instances) => {
     return instances
       .flatMap((network) =>
@@ -98,40 +93,22 @@ const ExplorePage = ({
       ));
   };
 
-  const isValidDaoURI = (daoURI) => {
-    // Ensure daoURI is a valid IPFS URI (ipfs:// or https://ipfs.io/ipfs/)
-    return (
-      (daoURI.startsWith("ipfs://") && daoURI.length >= 53) ||
-      (daoURI.startsWith("https://ipfs.io/ipfs/") && daoURI.length >= 67)
-    );
-  };
-
   const filteredRegistrationsSunrise = (sunriseInstances, networkFilterValue = "") => {
     return sunriseInstances
       .filter((registration) => {
-        //Exclude the specific DAO address
-        if (registration.daoAddress.toLowerCase() === "0xdeb9e5915db81011c549799a9ea37ede4d72efba") {
+        const daoAddressLower = registration.daoAddress.toLowerCase();
+        const excludedAddress = "0xdeb9e5915db81011c549799a9ea37ede4d72efba";
+
+        if (daoAddressLower === excludedAddress || !isValidDaoURI(registration.daoURI)) {
           return false;
         }
 
-        // Check if the daoURI is in a valid format
-        if (!isValidDaoURI(registration.daoURI)) {
-          return false;
-        }
-
-        // Apply network filter based on the networkFilterValue
-        if (registration.registrationNetwork.id === networkFilterValue) {
-          return true;
-        }
-
+        return registration.registrationNetwork.id === networkFilterValue;
       })
       .map((registration, i) => (
         <RegistrationLeanCard key={i} {...registration} />
       ));
   };
-
-  console.log("gnnosis", registrationInstances
-    .filter((reg) => NetworkFilterRegistrations(reg, "gnosis")))
 
   const renderCards = () => {
     switch (networkFilter) {
@@ -142,32 +119,23 @@ const ExplorePage = ({
       case "stargaze":
         return filteredRegistrations(stargazeInstances);
       case "arbitrum-one":
-        return filteredRegistrationsSunrise(sunriseNetworkInstances, "arbitrum-one")
+        return filteredRegistrationsSunrise(sunriseNetworkInstances, "arbitrum-one");
       case "chapel":
-        return filteredRegistrationsSunrise(sunriseNetworkInstances, "chapel")
-      // case "goerli":
-      //   return registrationInstances
-      //     .filter((reg) => NetworkFilterRegistrations(reg, "goerli"))
-      //     .map((registration, i) => (
-      //       <RegistrationCard key={i} {...registration} />
-      //     ));
+        return filteredRegistrationsSunrise(sunriseNetworkInstances, "chapel");
       case "gnosis":
-        return filteredRegistrationsSunrise(sunriseNetworkInstances, "gnosis")
+        return filteredRegistrationsSunrise(sunriseNetworkInstances, "gnosis");
       case "ethereum":
-        return filteredRegistrationsSunrise(sunriseNetworkInstances, "mainnet")
-
+        return filteredRegistrationsSunrise(sunriseNetworkInstances, "mainnet");
       case "optimism":
         return registrationInstances
           .filter(
             (reg) =>
               NetworkFilterRegistrations(reg, "optimism") &&
-              reg.daoAddress !== "0xdeb9e5915db81011c549799a9ea37ede4d72efba" &&
-              reg.id !== "0xdf732f01c22a0d06bf48f511ff6171b956bf9022" // Filter out specific daoAddress
+              reg.daoAddress !== "0xdeb9e5915db81011c549799a9ea37ede4d72efba"
           )
           .map((registration, i) => (
             <RegistrationCard key={i} {...registration} />
           ));
-
       case "optimism-goerli":
         return registrationInstances
           .filter((reg) => NetworkFilterRegistrations(reg, "optimism-goerli"))
@@ -184,73 +152,29 @@ const ExplorePage = ({
         return easAttestations
           .filter(
             (attestation) =>
-              !filterEASbyAttester.includes(attestation.attester) && // Filter out by attester
-              !filterEASbyId.includes(attestation.id) // Filter out by attestation ID
+              !filterEASbyAttester.includes(attestation.attester) &&
+              !filterEASbyId.includes(attestation.id)
           )
           .map((attestation, i) => (
             <AttestationCard key={i} {...attestation} />
           ));
-
       case "ensTextRecords":
         return ENSTextRecords.map((textRecord, i) => (
           <ENSCard key={i} {...textRecord} />
         ));
       default:
         return (
-          <>
-            <div className="dao-cards">{daoCards}</div>
-            <br></br>
-            <div className="dao-cards">{daodaoCards}</div>
-            <br></br>
-            <div className="dao-cards">{osmosisDaoCards}</div>
-            <br></br>
-            <div className="dao-cards">{stargazeDaoCards}</div>
-          </>
+          <div className="dao-cards-all">
+            {daoCards}
+          </div>
         );
     }
   };
 
-  // Handle EVM networks
   const daoCards = registrationInstances
     .filter((reg) => filterRegistrations(reg, filterVal))
     .map((registration, i) => {
       return <RegistrationCard key={i} {...registration} />;
-    });
-
-  // Handle Juno DAOs
-  const daodaoCards = junosInstances
-    .flatMap((network) =>
-      network.registrationNetwork.registrations.filter((reg) =>
-        filterRegistrations(reg, filterVal)
-      )
-    )
-    .map((registration, i) => {
-      return <RegistrationCard key={i} {...registration} standalone={true}
-        displayWithoutEdit={true} />;
-    });
-
-  // Handle Stargaze DAOs
-  const stargazeDaoCards = stargazeInstances
-    ?.flatMap((network) =>
-      network.registrationNetwork.registrations.filter((reg) =>
-        filterRegistrations(reg, filterVal)
-      )
-    )
-    .map((registration, i) => {
-      return <RegistrationCard key={i} {...registration} standalone={true}
-        displayWithoutEdit={true} />;
-    });
-
-  // Handle Osmosis DAOs
-  const osmosisDaoCards = osmosisInstances
-    ?.flatMap((network) =>
-      network.registrationNetwork.registrations.filter((reg) =>
-        filterRegistrations(reg, filterVal)
-      )
-    )
-    .map((registration, i) => {
-      return <RegistrationCard key={i} {...registration} standalone={true}
-        displayWithoutEdit={true} />;
     });
 
   return (
@@ -268,15 +192,15 @@ const ExplorePage = ({
               key={index}
               text={button.text}
               onClick={() => setNetworkFilter(button.filter)}
-              className={
-                networkFilter === button.filter ? "button-highlighted" : ""
-              }
+              className={networkFilter === button.filter ? "button-highlighted" : ""}
             />
           ))}
         </div>
       </div>
 
-      <div className="dao-cards">{renderCards()}</div>
+      <div className={networkFilter === "all" ? "dao-cards-all" : "dao-cards"}>
+        {renderCards()}
+      </div>
     </div>
   );
 };
