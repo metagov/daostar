c7b382303112dcc516492e1eb06d544ab74c5a70
Rashmi-278 <rashmiabbigeri@gmail.com>
2025-02-24 12:32:21 +0530

    add op sepolia

--- daostar-website/src/App.js
+++ daostar-website/src/App.js
@@ -182,11 +182,11 @@ console.log(mainnetData);
     context: { apiName: "goerli" },
     variables: { id: "goerli" },
   });
+  const optimismSepoliaRes = useQuery(queries.REGISTRATIONS, {
+    context: { apiName: "optimismSepolia" },
+    variables: { id: "optimism-sepolia" },
+  });
   // TODO: Replace Goerli with Sepolia
-  // const optimismGoerliRes = useQuery(queries.REGISTRATIONS, {
-  //   context: { apiName: "optimismGoerli" },
-  //   variables: { id: "optimism-goerli" },
-  // });
   // const arbitrumGoerliRes = useQuery(queries.REGISTRATIONS, {
   //   context: { apiName: "arbitrumGoerli" },
   //   variables: { id: "arbitrum-goerli" },
@@ -219,11 +219,11 @@ console.log(mainnetData);
     data: optimismData,
   } = optimismRes;
 
-  // const {
-  //   loading: optimismGoerliLoading,
-  //   error: optimismGoerliError,
-  //   data: optimismGoerliData,
-  // } = optimismGoerliRes;
+  const {
+    loading: optimismSepoliaLoading,
+    error: optimismSepoliaError,
+    data: optimismSepoliaData,
+  } = optimismSepoliaRes;
   // // const {
   // //   loading: arbitrumGoerliLoading,
   // //   error: arbitrumGoerliError,
@@ -300,8 +300,7 @@ console.log(mainnetData);
   if (
     error ||
     goerliError ||
-    // optimismGoerliError ||
-    // arbitrumGoerliError ||
+    optimismSepoliaError ||
     chapelError ||
     optimismError ||
     mainnetv0Error ||
@@ -310,8 +309,7 @@ console.log(mainnetData);
     console.error("Mainnet Error " + error);
     console.error("Mainnet v0 Error " + mainnetv0Error);
     console.error("Goerli Error " + goerliError);
-    // console.error("Optimism Goerli Error " + optimismGoerliError);
-    // console.error("Arbitrum Goerli Error" + arbitrumGoerliError);
+    console.error("Optimism Sepolia Error " + optimismSepoliaError);
     console.error("Chapel Error" + chapelError);
     console.error("Optimism Error" + optimismError);
     console.error("Arbitrum Error" + arbitrumError);
@@ -321,8 +319,7 @@ console.log(mainnetData);
     loading ||
     goerliLoading ||
     gnosisLoading ||
-    // optimismGoerliLoading ||
-    // arbitrumGoerliLoading ||
+    optimismSepoliaLoading ||
     chapelLoading ||
     optimismLoading ||
     arbitrumLoading
@@ -334,14 +331,13 @@ console.log(mainnetData);
     mainnetv0Data?.registrationNetwork?.registrations || [];
   const goerliRegistrations =
     goerliData?.registrationNetwork?.registrations || [];
- // const optimismGoerliRegistrations =
-  //  optimismGoerliData?.registrationNetwork?.registrations || [];
+ const optimismSepoliaRegistrations =
+  optimismSepoliaData?.registrationNetwork?.registrations || [];
   const optimismRegistrations =
     optimismData?.registrationNetwork?.registrations || [];
   const gnosisRegistrations =
     gnosisData?.registrationNetwork?.registrations  || [];
-  // const arbitrumGoerliRegistrations =
-  //   arbitrumGoerliData?.registrationNetwork?.registrations || [];
+  
   const arbitrumRegistrations =
     arbitrumData?.registrationNetwork?.registrations || [];
   const chapelRegistrations =
@@ -370,13 +366,12 @@ console.log(mainnetData);
   const allRegistrationInstances = mainnetRegistrations.concat(
     allMainnetV0Registrations,
     goerliRegistrations,
+    optimismSepoliaRegistrations,
     //arbitrumGoerliRegistrations,
-    optimismRegistrations,
-    // arbitrumRegistrations
+    arbitrumRegistrations
   );
 
   const EASAttestations = EASOptimismAttestations.concat(EASOptimismSepoliaAttestations);
-  const daodaoRegInstances = daodaoInstances;
   const registrationInstances = allRegistrationInstances.filter(
     (instance) => !registrationIdsToFilter.includes(instance.id)
   );
@@ -384,7 +379,9 @@ console.log(mainnetData);
   const sunriseNetworkInstances = gnosisRegistrations.concat(
     chapelRegistrations,
     mainnetRegistrations,
-    arbitrumRegistrations
+    arbitrumRegistrations,
+    optimismRegistrations,
+
   );
 
   console.log({
@@ -392,7 +389,7 @@ console.log(mainnetData);
     mainnetv0Data,
     goerliData,
     gnosisData,
-    // optimismGoerliData,
+    optimismSepoliaData,
     // arbitrumGoerliData,
     chapelData,
     arbitrumData,
